746e2e49e7a35e14a1b9061b4f0abd9576ff9d4c
Jiyouc <45361851+Jiyouc@users.noreply.github.com>
2019-12-01 13:07:35 +0800

    add post method

--- src/main/java/com/example/restful_demo/controller/BookController.java
+++ src/main/java/com/example/restful_demo/controller/BookController.java
@@ -1,22 +1,36 @@
 package com.example.restful_demo.controller;
 
+import com.example.restful_demo.mapper.BookMapper;
 import com.example.restful_demo.model.Book;
 import com.example.restful_demo.Service.BookService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-public class HelloController {
+public class BookController {
 
     @Autowired
     BookService bookService;
 
-    @GetMapping("/book/all")
+    @Autowired
+    BookMapper bookMapper;
+
+    @GetMapping("/book")
     public List<Book> showBooks() {
         List<Book> bookList = bookService.selectAllBooks();
         return bookList;
+    }
+
+    @PostMapping("/book")
+    public Object insertBook(Book book) {
+        Book record = new Book();
+        BeanUtils.copyProperties(book,record);
+        bookMapper.insert(record);
+        return null;
     }
 }
